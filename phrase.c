4a85a24a970784cc9ed35aa93534f7ad48c89191
Quezzar <98542233+Quezzar@users.noreply.github.com>
2022-11-12 17:06:00 +0100

    Création des fichiers phrases.c et phrases.h

---
+++ phrase.c
@@ -0,0 +1,5 @@
+//
+// Created by maxim on 12/11/2022.
+//
+
+#include "phrase.h"
---
+++ phrase.h
@@ -0,0 +1,8 @@
+//
+// Created by maxim on 12/11/2022.
+//
+
+#ifndef PROJET_MOT_PHRASE_H
+#define PROJET_MOT_PHRASE_H
+
+#endif //PROJET_MOT_PHRASE_H
