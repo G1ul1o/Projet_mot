25d81ddb6e6f66192c2b88374cfbfdce1dd18588
G1ul1o <103744749+G1ul1o@users.noreply.github.com>
2022-11-21 00:10:51 +0100

    Update tree.c

--- tree.c
+++ tree.c
@@ -1,11 +1,9 @@
-//
-// Created by giuga on 31/10/2022.
-//
 #include <stdlib.h>
 #include <stdio.h>
 #include "tree.h"
 
-t_tree CreateEmptyTree()
+//crer un arbre vide et elle ne prend rien en paramètre
+t_tree CreeArbrevide()
 {
     t_tree arbre;
     for (int i = 0; i < 26; ++i) {
@@ -15,23 +13,26 @@ t_tree CreateEmptyTree()
     return arbre;
 }
 
+//cherche lettre dans le root et elle prend en paramètre le tree, lettre, l'indice du noeud a tester
 p_node CherchelettreRoot(t_tree pn,char lettre,int i)
 {
     t_tree temp;
     temp=pn;
     p_node tempnode;
-    if(temp.root[i]==NULL)
+
+    if(temp.root[i]==NULL) //si l'enfant est NULL alors on retourne NULL car l'arbre ne contient pas la lettre
     {
         return NULL;
     }
-    if(temp.root[i]->value==lettre)
+
+    if(temp.root[i]->value==lettre)//si l'enfant contient la lettre alors on le retourne
     {
         return temp.root[i];
     }
 
     if(i+1< temp.nombre_pointeurs)
     {
-        tempnode= CherchelettreRoot(pn,lettre,i+1);
+        tempnode= CherchelettreRoot(pn,lettre,i+1); //on teste le suivant
         if(tempnode!=NULL)
         {
             return tempnode;
@@ -41,48 +42,51 @@ p_node CherchelettreRoot(t_tree pn,char lettre,int i)
             return NULL;
         }
     }
-
-
     return NULL;
 }
 
-t_tree createfromEmptyTreeFromNode(char mot[35],char type[35])
+//On ajoute le premier noeud à un arbre vide elle prend en paramètre le mot et le type
+t_tree PremierNoeud(char mot[35],char type[35])
 {
     t_tree ps;
-    ps=CreateEmptyTree();
-    ps.root[0] = Creearbre(ps.root[0],mot,0,type);
+    ps=CreeArbrevide();
+    ps.root[0] = AjoutNoeud(ps.root[0],mot,0,type);
     ps.nombre_pointeurs=1;
     return ps;
 }
 
-t_tree createTreeFromNode(t_tree t,char mot[35],char type[35])
+//On ajoute un noeud à un arbre, elle prends en paramètre l'arbre, le mot et le type
+t_tree AjoutNoeudArbre(t_tree t,char mot[35],char type[35])
 {
     p_node temp;
-    temp= CherchelettreRoot(t,mot[0],0);
+    temp= CherchelettreRoot(t,mot[0],0);  //on teste si la première lettre est dans l'arbre
     if (temp!=NULL)
     {
-        temp= Creearbre(temp,mot,1,type);
+        temp= AjoutNoeud(temp,mot,1,type); //si oui on se déplace dans le noeud
     }
     else
     {
-        t.root[t.nombre_pointeurs] = Creearbre(t.root[t.nombre_pointeurs],mot,0,type);
+        t.root[t.nombre_pointeurs] = AjoutNoeud(t.root[t.nombre_pointeurs],mot,0,type); //sinon on crée le noeud
         t.nombre_pointeurs+=1;
     }
 
     return t;
 }
+
+//On recherche le fléchie d'un mot, elle prend en paramètre l'arbre et le mot
 void recherche_flechie(t_tree arbre, char mot[25])
 {
     int i = 0, taille_mot = 0;
     int trouve = 0, num_cell = 0;
     p_node pn;
-    p_node temp= CherchelettreRoot(arbre,mot[0],0);
+    p_node temp= CherchelettreRoot(arbre,mot[0],0); //on teste si la première lettre existe et on se place dessus
 
     if(temp!=NULL)
     {
         pn =trouver_flechie(temp, mot);
     }
 
+    //on teste si le mot existe dans les flèchies de notre cellule
     if(pn!=NULL)
     {
         p_cell cherche_mot = pn->pointeur;
@@ -124,7 +128,8 @@ void recherche_flechie(t_tree arbre, char mot[25])
     if(trouve==1)
     {
         p_cell flechie = pn->pointeur;
-        for (int indice = 0; indice < num_cell; indice++) {
+        for (int indice = 0; indice < num_cell; indice++)
+        {
             flechie = flechie->next;
         }
         printf("%s %s %s\n", flechie->mot, flechie->type, pn->mot_base);
@@ -172,7 +177,6 @@ void type_mot(char lemot[35], t_tree arbre_nom, t_tree arbre_adj, t_tree arbre_v
     }
     montemp2 = CherchelettreRoot(arbre_adv,lemot[0],0);
     int lecompteur3=0;
-
     if (montemp2!=NULL)
     {
         lecompteur3 = compteur(montemp2, lemot);
@@ -186,4 +190,5 @@ void type_mot(char lemot[35], t_tree arbre_nom, t_tree arbre_adj, t_tree arbre_v
     {
         printf("Le mot n'existe pas.\n");
     }
+
 }
