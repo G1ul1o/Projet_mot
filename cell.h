8e6451f0bf590b9c6ec78855add60247ebb51b87
G1ul1o <103744749+G1ul1o@users.noreply.github.com>
2022-11-21 00:03:42 +0100

    Update cell.h

--- cell.h
+++ cell.h
@@ -1,25 +1,13 @@
-//
-// Created by giuga on 07/11/2022.
-//
-
 #ifndef PROJET_MOT_CELL_H
 #define PROJET_MOT_CELL_H
 struct s_cell
 {
     char mot[30];
-    char type[50];
+    char type[30];
     struct s_cell *next;
 };
 
 typedef struct s_cell t_cell, *p_cell;
-
-struct s2_cell
-{
-    char mot[30];
-    struct s2_cell *next;
-};
-
-typedef struct s2_cell t2_cell, *p2_cell;
 
 typedef struct flechie
 {
@@ -27,8 +15,8 @@ typedef struct flechie
     char type_flechies[50];
 } flechies;
 
-p_cell createCellflechie(char [30],char [30]);
+p_cell CreationCellFlechie(char [30],char [30]);
 
-p2_cell createCell(char [30]);
 
 #endif //PROJET_MOT_CELL_H
+
