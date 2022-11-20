6e773511c1f1f7603ea6ae52058f5a6d8590758f
G1ul1o <103744749+G1ul1o@users.noreply.github.com>
2022-11-21 00:01:27 +0100

    Update main.c

--- main.c
+++ main.c
@@ -22,44 +22,49 @@ int main()
     int creation_arbreadv = 0;
     int creation_arbreverb = 0;
 
+    int choix = 0,choix2=0;
+
+    //on crée l'arbre
     if (dicofile != NULL)
     {
         while (fscanf(dicofile, "%s\t%s\t%s", flechie, base, formes) !=
-               EOF) //EOF pour end of file et \t pour les tabulations
-            // char *base_arbre=(char*)malloc(35*sizeof(char));
+               EOF) //EOF pour end of file et \t pour les tabulations et fscanf nous permet de parcourir le fichier ligne par ligne
         {
             for (int j = 0; j < 3; ++j) {
-                type[j] = formes[j];
+                type[j] = formes[j]; //on récupère le type de notre mot (Nom,Ver,Adv,Adj)
             }
             type[3]='\0';
-            if (type[0] == 'N') {
+
+            //on va ensuite ajouter le mot à l'arbre correspondant à son type
+            if (type[0] == 'N')
+            {
                 if (creation_arbrenom == 0) {
-                    arbre_nom = createfromEmptyTreeFromNode(base,type);
+                    arbre_nom = PremierNoeud(base,type);
                     creation_arbrenom++;
                 } else {
-                    arbre_nom = createTreeFromNode(arbre_nom, base,type);
+                    arbre_nom = AjoutNoeudArbre(arbre_nom, base,type);
                 }
             } else if (type[0] == 'V') {
                 if (creation_arbreverb == 0) {
-                    arbre_ver = createfromEmptyTreeFromNode(base,type);
+                    arbre_ver = PremierNoeud(base,type);
                     creation_arbreverb++;
                 } else {
-                    arbre_ver = createTreeFromNode(arbre_ver, base,type);
+                    arbre_ver = AjoutNoeudArbre(arbre_ver, base,type);
                 }
             } else if (type[0] == 'A') {
                 if (type[2] == 'v') {
                     if (creation_arbreadv == 0) {
-                        arbre_adv = createfromEmptyTreeFromNode(base,type);
+                        arbre_adv = PremierNoeud(base,type);
                         creation_arbreadv++;
                     } else {
-                        arbre_adv = createTreeFromNode(arbre_adv, base,type);
+                        arbre_adv = AjoutNoeudArbre(arbre_adv, base,type);
                     }
                 } else {
                     if (creation_arbreadj == 0) {
-                        arbre_adj = createfromEmptyTreeFromNode(base,type);
+                        arbre_adj = PremierNoeud(base,type);
                         creation_arbreadj++;
                     } else {
-                        arbre_adj = createTreeFromNode(arbre_adj, base,type);
+                        arbre_adj = AjoutNoeudArbre(arbre_adj, base,type);
                     }
                 }
 
@@ -69,20 +74,23 @@ int main()
     }
     fclose(dicofile);
 
-    int choix = 0,choix2=0;
 
+    //on rentre dans le menu
     printf("Bonjours et bienvenue, veuillez choisir ce que vous voulez faire :\n");
-    
+
     while(choix != 4) //permet de stopper le programme quand le chiffre est saisie
     {
         do
         {
+            choix=0;
+            choix2=0;
             printf("Que voulez vous faire ?\n"
-                   "1 : Verifier si un mot existe\n"
+                   "1 : Verifier si un mot existe (forme basique)\n"
                    "2 : Generer une phrase\n"
                    "3 : Chercher une forme flechie\n"
                    "4 : Quitter\n");
             scanf("%d", &choix);
+
             if (choix == 1) {
                 printf("Veuillez saisir un mot (sous forme basique): \n");
                 char lemot[35];
@@ -91,67 +99,69 @@ int main()
             }
             else if (choix == 2)
             {
-                do {
-                    printf ("Que voulez vous faire ?\n"
-                            "1 : Phrase type 1 (nom verbe adjectif nom) ?\n"
-                            "2 : Phrase type 2 (nom qui verbe verbe adjectif nom adjectif) ?\n"
-                            "3 : Phrase type 3 (nom verbe adverbe adjectif) ?\n"
-                            "4 : Quitter\n");
+                while(choix2 != 4)
+                {
+                    do {
+                        printf("Que voulez vous generer comme phrase ?\n"
+                               "1 : Phrase type 1 (nom verbe adjectif nom) ?\n"
+                               "2 : Phrase type 2 (nom qui verbe verbe adjectif nom adjectif) ?\n"
+                               "3 : Phrase type 3 (nom verbe adverbe adjectif) ?\n"
+                               "4 : Quitter\n");
 
-                    scanf("%d", &choix2);
-                    if(choix2==1)
-                    {
-                        type1(arbre_nom,arbre_ver,arbre_adj);
-                    }
-                    else if (choix2==2)
-                    {
-                        type2(arbre_nom,arbre_ver,arbre_adj);
-                    }
-                    else if (choix2==3)
-                    {
-                        type3(arbre_nom,arbre_ver,arbre_adj,arbre_adv);
-                    }
-                } while (choix2!=(1,2,3));
-
+                        scanf("%d", &choix2);
+                        if (choix2 == 1) {
+                            phrase_modele1(arbre_nom, arbre_ver, arbre_adj);
+                        } else if (choix2 == 2) {
+                            phrase_modele2(arbre_nom, arbre_ver, arbre_adj);
+                        } else if (choix2 == 3) {
+                            phrase_modele3(arbre_nom, arbre_ver, arbre_adj, arbre_adv);
+                        }
+                    } while (choix2 != (1, 2, 3,4));
+                }
 
             }
             else if (choix == 3)
             {
-                printf ("Que voulez vous faire ?\n"
-                       "1 : Chercher un nom ?\n"
-                       "2 : Chercher un verbe ?\n"
-                       "3 : Chercher un adj ?\n"
-                       "4 : Chercher un adv ?\n"
-                       "5 : Quitter\n");
-
-                scanf("%d", &choix2);
-
-                char lemot[35];
-                printf("Veuillez saisir un mot : \n");
-                scanf("%s", &lemot);
-
-                if(choix2==1)
+                while(choix2 != 5)
                 {
-                    recherche_flechie(arbre_nom,lemot);
-                }
-                else if (choix2==2)
-                {
-                    recherche_flechie(arbre_ver,lemot);
-                }
-                else if (choix2==3)
-                {
-                    recherche_flechie(arbre_adj,lemot);
-                }
-                else if(choix2==4)
-                {
-                    recherche_flechie(arbre_adv, lemot);
-                }
+                    do
+                    {
+                        printf ("Que voulez vous chercher ?\n"
+                                "1 : Chercher un nom ?\n"
+                                "2 : Chercher un verbe ?\n"
+                                "3 : Chercher un adj ?\n"
+                                "4 : Chercher un adv ?\n"
+                                "5 : Quitter\n");
+                        scanf("%d", &choix2);
+                        char lemot[35];
 
+                        if(choix2!=5)
+                        {
+                            printf("Veuillez saisir un mot : \n");
+                            scanf("%s", &lemot);
+                        }
+
+                        if(choix2==1)
+                        {
+                            recherche_flechie(arbre_nom,lemot);
+                        }
+                        else if (choix2==2)
+                        {
+                            recherche_flechie(arbre_ver,lemot);
+                        }
+                        else if (choix2==3)
+                        {
+                            recherche_flechie(arbre_adj,lemot);
+                        }
+                        else if(choix2==4)
+                        {
+                            recherche_flechie(arbre_adv, lemot);
+                        }
+                    }while (choix2 != (1, 2, 3,4,5));
+                }
             }
         }while(choix!=(1,2,3,4)); //saisir sécurisée
     }
 
-    char* test =type1(arbre_nom,arbre_ver,arbre_adj);
-    printf("%s",test);
     return 0;
 }
